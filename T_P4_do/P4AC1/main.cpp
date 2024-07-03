62832c525ba6f8ec25c708fde506e4250af90ffe
LoncheDePierna <diego.maldonado@uniat.edu.mx>
2024-07-02 22:03:00 -0600

    AC1

--- T_P4_do/P4AC1/main.cpp
+++ T_P4_do/P4AC1/main.cpp
@@ -1,84 +1,107 @@
 #include <iostream>
 #include <vector>
-
 using namespace std;
 
-// Función para crear la matriz de adyacencia
-void crearMatrizAdyacencia(int **matriz, int n, vector<pair<int, int>> aristas) {
-    // Inicializar la matriz de adyacencia con 0
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            matriz[i][j] = 0;
+// Estructura de datos para almacenar un borde de graph
+struct Edge {
+    int src, dest;
+};
+
+// Una clase para representar un objeto graph
+class Graph
+{
+public:
+    // un vector de vectores para representar una lista de adyacencia
+    vector<vector<int>> adjList;
+
+    // una matriz de adyacencia
+    vector<vector<int>> adjMatrix;
+
+    // Constructor de graph
+    Graph(vector<Edge> const &edges, int n)
+    {
+        // cambiar el tamaño del vector para contener `n` elementos de tipo `vector<int>`
+        adjList.resize(n);
+
+        // cambiar el tamaño de la matriz para contener `n`x`n` elementos
+        adjMatrix.resize(n, vector<int>(n, 0));
+
+        // agrega bordes al grafo dirigido
+        for (auto &edge: edges)
+        {
+            // insertar al final
+            adjList[edge.src - 1].push_back(edge.dest - 1);
+
+            // actualizar matriz de adyacencia
+            adjMatrix[edge.src - 1][edge.dest - 1] = 1;
+
+            // elimina el comentario del siguiente código para el graph no dirigido
+            // adjList[edge.dest].push_back(edge.src);
+            // adjMatrix[edge.dest][edge.src] = 1;
         }
     }
 
-    // Completar la matriz de adyacencia con 1 para las aristas existentes
-    for (pair<int, int> arista : aristas) {
-        matriz[arista.first][arista.second] = 1;
-        matriz[arista.second][arista.first] = 1; // Para grafos no dirigidos
+    // Función para imprimir la matriz de adyacencia
+    void printAdjMatrix() const
+    {
+        cout << endl << "Matriz de Adyacencia:" << endl;
+        for (const auto &row: adjMatrix)
+        {
+            for (int val: row)
+            {
+                cout << val << " ";
+            }
+            cout << endl;
+        }
     }
-}
+};
 
-// Función para imprimir la matriz de adyacencia
-void imprimirMatrizAdyacencia(int **matriz, int n) {
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            cout << matriz[i][j] << " ";
+// Función para imprimir la representación de la lista de adyacencia de un graph
+void printGraph(Graph const &graph, int n)
+{
+    cout << "Lista de Adyacencia:" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        // imprime el número de vértice actual
+        cout << i + 1 << " --> ";
+
+        // imprime todos los vértices vecinos de un vértice `i`
+        for (int v: graph.adjList[i]) {
+            cout << v + 1 << " ";
         }
         cout << endl;
     }
 }
 
-int main() {
+// Implementación de graph usando STL
+int main()
+{
+    // vector de los bordes del graph según el diagrama anterior.
+    // Tenga en cuenta que el vector de inicialización en el siguiente formato
+    // funciona bien en C++11, C++14, C++17 pero fallará en C++98.
+    vector<Edge> edges =
+            {
+                    {1, 2}, {1, 3}, {1, 6},
+                    {2, 1}, {2, 5},
+                    {3, 1}, {3, 4}, {3, 6},
+                    {4, 3}, {4, 5}, {4, 6}, {4, 7}, {4, 8},
+                    {5, 2}, {5, 4}, {5, 6}, {5, 7},
+                    {6, 1}, {6, 3}, {6, 4}, {6, 5},
+                    {7, 4}, {7, 5}, {7, 8},
+                    {8, 4}, {8, 7}
+            };
 
-    vector<pair<int, int>> Grafo =
-    {
-            {1, 2}, {1, 3}, {1, 6},
-            {2, 1}, {2, 5},
-            {3, 1}, {3, 4}, {3, 6},
-            {4, 3}, {4, 5}, {4, 6}, {4, 7}, {4, 8},
-            {5, 2}, {5, 4}, {5, 6}, {5, 7},
-            {6, 1}, {6, 3}, {6, 4}, {6, 5},
-            {7, 4}, {7, 5}, {7, 8}
-            {8, 4}, {8, 7}
-    };
-            /*
-            a1
-            b2
-            c3
-            d4
-            e5
-            f6
-            g7
-            h8
-            {a, b}, {a, c}, {a, f},
-            {b, a}, {b, e},
-            {c, a}, {c, d}, {c, f},
-            {d, c}, {d, e}, {d, f}, {d, g}, {d, h},
-            {e, b}, {e, d}, {e, f}, {e, g},
-            {f, a}, {f, c}, {f, d}, {f, e},
-            {g, d}, {g, e}, {g, h}
-            {h, d}, {h, g}
-             */
-    };
+    // número total de nodos en el graph (etiquetados de 1 a 8)
+    int n = 8;
 
-    // Crear la matriz de adyacencia
-    int **matriz = new int*[n];
-    for (int i = 0; i < n; i++) {
-        matriz[i] = new int[n];
-    }
+    // construir grafo
+    Graph graph(edges, n);
 
-    crearMatrizAdyacencia(matriz, n, aristas);
+    // imprime la representación de la lista de adyacencia de un graph
+    printGraph(graph, n);
 
-    // Imprimir la matriz de adyacencia
-    cout << "Matriz de adyacencia:" << endl;
-    imprimirMatrizAdyacencia(matriz, n);
-
-    // Eliminar la memoria asignada dinámicamente
-    for (int i = 0; i < n; i++) {
-        delete[] matriz[i];
-    }
-    delete[] matriz;
+    // imprime la matriz de adyacencia
+    graph.printAdjMatrix();
 
     return 0;
 }
